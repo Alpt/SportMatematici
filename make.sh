d61e24e4e2c134bb0ae6289a67e91482c183b541
andrea <andrea@taccuino>
2019-10-14 23:18:51 +0200

    Another make

--- make.sh
+++ make.sh
@@ -4,13 +4,15 @@ echo git commit -a -m 'Another make'
 git commit -a -m 'Another make'
 
 for i in `ls -1 *.tex`; do
-	if [ "$i" == "gpl.tex" ]; then continue; fi
 
-#latex  --interaction nonstopmode $i &> /dev/null
-#latex  --interaction nonstopmode $i &> /dev/null && \
-#echo DVI compiled
+	
+	if [ "$i" = "gpl.tex" ]; then continue; fi
 
+	echo $i
+	#latex  --interaction nonstopmode $i &> /dev/null
+	#latex  --interaction nonstopmode $i &> /dev/null && \
+	#echo DVI compiled
 
-pdflatex --interaction nonstopmode $i &> /dev/null
-pdflatex --interaction nonstopmode $i &> /dev/null && pdflatex $i &> /dev/null && echo PDF compiled
+	pdflatex --interaction nonstopmode $i &> /dev/null
+	pdflatex --interaction nonstopmode $i &> /dev/null && pdflatex $i &> /dev/null && echo PDF compiled
 done
