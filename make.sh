78adcef30f9c7229e9561fda0df96650f786edf6
andrea <andrea@email.domain>
2022-01-03 09:42:40 +0100

    M .gitignore A  compile.sh  M make.sh

--- make.sh
+++ make.sh
@@ -3,16 +3,4 @@
 echo git commit -a -m 'Another make'
 git commit -a -m 'Another make'
 
-for i in `ls -1 *.tex`; do
-
-	
-	if [ "$i" = "gpl.tex" ]; then continue; fi
-
-	echo $i
-	#latex  --interaction nonstopmode $i &> /dev/null
-	#latex  --interaction nonstopmode $i &> /dev/null && \
-	#echo DVI compiled
-
-	pdflatex --interaction nonstopmode $i 
-	pdflatex --interaction nonstopmode $i &> /dev/null && pdflatex $i &> /dev/null && echo PDF compiled
-done
+./compile.sh
